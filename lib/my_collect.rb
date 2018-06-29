c3ae4a1ddf110c5a35fae30e789cbd9a5c392b38
Davidlago104 <davidlago104@gmail.com>
2018-06-29 18:36:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,3 @@
 def my_collect(collection)
-  my_collect(collection) do |name|
-    name.split(" ").first 
-    
- i = 0 
- while i < collection.length 
-  yield collection[i]
-  i += 1 
-  
-  end
-end
-collection
+
 end
